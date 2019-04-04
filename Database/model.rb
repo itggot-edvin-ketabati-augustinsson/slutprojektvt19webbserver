fd766574c3d5fb0608ce3b57338727ff86244396
itggot-edvin-ketabati-augustinsson <edvket@gmail.com>
2019-04-04 10:21:23 +0200

    Fully functional login and register added

--- Database/model.rb
+++ Database/model.rb
@@ -1,11 +1,33 @@
 require 'sqlite3'
 
 def open_db_link()
-    db = SQLite3::Database.new('qna.db')
+    db = SQLite3::Database.new('Database/qna.db')
     db.results_as_hash = true
+    return db
 end
 
 def open_db_link_nohash()
-    db = SQLite3::Database.new('qna.db')
+    db = SQLite3::Database.new('Database/qna.db')
     db.results_as_hash = false
+    return db
+end
+
+def register(name, pass)
+    db = open_db_link()
+    password = BCrypt::Password.create(pass)
+    db.execute("INSERT INTO users(Username, Password) VALUES( (?), (?) )",name, password)
+end
+
+def login(name, pass)
+    db = open_db_link()
+    result = db.execute("SELECT Password, UserId FROM users WHERE Username =(?)", name)
+    encrypted_pass = result[0]["Password"]
+    if BCrypt::Password.new(encrypted_pass) == pass
+        session[:loggedin] = true
+        session[:user_id] = result[0]["UserId"]
+        session[:name] = name
+        return true
+    else
+        return false
+    end
 end
--- controller.rb
+++ controller.rb
@@ -4,6 +4,29 @@ require 'sqlite3'
 require 'bcrypt'
 require_relative './Database/model.rb'
 
+enable :sessions
+
 get('/') do
     slim(:"Profile/login")
+end
+
+get('/register') do
+    slim(:"Profile/register")
+end
+
+post('/create') do
+    register(params["name"], params["pass"])
+    redirect('/')
+end
+
+post('/login') do
+    if login(params["name"], params["pass"]) == true
+        redirect('/profile')
+    else
+        redirect('/error')
+    end
+end
+
+get('/error') do
+    slim(:"Error/error")
 end
