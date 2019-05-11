cb6fa9d1e477cc1e61938a6afca66e276e8c42d5
itggot-edvin-ketabati-augustinsson <edvket@gmail.com>
2019-05-12 01:07:51 +0200

    Began documenting routes

--- controller.rb
+++ controller.rb
@@ -19,14 +19,25 @@ before do
     end
 end
 
+# Displays first page
+#
 get('/') do
     slim(:"Profile/login")
 end
 
+# Displays register view
+#
 get('/register') do
     slim(:"Profile/register")
 end
 
+# Creates a new account and validates input data, redirects to /
+#
+# @param [String] name, The username of the account 
+# @param [String] pass, The unencrypted password inputted
+# @param [String] repeat_pass, The unencrypted password inputted second time by user
+#
+# See Model#User#register
 post('/create_account') do
     result = User.register(params["name"], params["pass"], params["repeat_pass"])
     if result[:error] == false
@@ -36,6 +47,12 @@ post('/create_account') do
     end
 end
 
+# Checks input data and logs user in, redirects to /profile
+#
+# @param [String] name, The username of the account 
+# @param [String] pass, The unencrypted password inputted
+#
+# See Model#User#login
 post('/login') do
     loginfo = User.login(params["name"], params["pass"])
     if loginfo[:loggedin] == true
@@ -47,6 +64,11 @@ post('/login') do
     end
 end
 
+# Displays the users profile page and questions
+#
+# @param [Integer] user_id, The users unique ID
+#
+# See Model#Question#get_asked_questions
 get('/profile') do
     questions = Question.get_asked_questions(session[:user_id])
     slim(:"Profile/profile", locals:{
@@ -54,11 +76,20 @@ get('/profile') do
     })
 end
 
+# Logs the user out, redirects to /
+#
 post('/logout') do
     session.destroy
     redirect('/')
 end
 
+# Asks another user the inputted question, redirects to /profile
+#
+# @param [Integer] from_id, Unique ID of the user asking
+# @param [Integer] to_id, Unique ID of the user being asked
+# @param [String] question, The question being asked
+#
+# See Model#Question#ask
 post('/ask/:id') do
    from_id = session[:user_id]
    to_id = params["id"].to_i
@@ -71,6 +102,12 @@ post('/ask/:id') do
     end
 end
 
+# Answers a users question, redirects to /recieved
+#
+# @param [Integer] question_id, The unique ID of the question being answered
+# @param [String] answer, The contents of the answer
+#
+# See Model#Question#answer_question
 post('/answer/:id') do 
     question_id = params["id"].to_i
     answer = params["answer"]
@@ -78,6 +115,11 @@ post('/answer/:id') do
     redirect('/recieved')
 end
 
+# Displays all questions asked to a user
+#
+# @param [Integer] user_id, The unique ID of the user whose questions are displayed
+#
+# See Model#Question#get_questions
 get('/recieved') do
     questions = Question.get_questions(session[:user_id])
     slim(:"Questions/recieved", locals:{
